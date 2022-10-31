57c7ecc54efe08e68a356b8d8d03efedcdb519dd
Guillaume RICHARD <guillaume.richard@wishgroupe.com>
2022-10-31 17:59:37 +0100

    Basic implementation based in trollfot's fork

--- README.rst
+++
@@ -1,3 +0,0 @@
-Kavalkade : a [knappe](https://github.com/HorsemanWSGI/knappe) powered tabletop-roleplaying tool.
-
-Features : None ! (But I'm working on it)
--- woip.py
+++ woip.py
@@ -1,6 +1,29 @@
-from kavalkade import Kavalkade
+from minicli import cli, run
 
-app = Kavalkade()
+
+def kavalkade_web():
+    from src.kavalkade import Kavalkade
+    from tinydb import TinyDB
+    return Kavalkade(
+        database=TinyDB('./data/kavalkade.json')
+    )
+
+
+@cli
+def http(debug: bool = False):
+    import asyncio
+    from aiowsgi import create_server
+
+    app = create_web_app()
+
+    loop = asyncio.new_event_loop()
+    wsgi_server = create_server(app, loop=loop, port=8000)
+
+    try:
+        wsgi_server.run()
+    finally:
+        loop.stop()
+
+
 if __name__ == '__main__':
-    import bjoern
-    bjoern.run(app, "127.0.0.1", 8000)
+    run()
